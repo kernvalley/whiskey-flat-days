d2a345ff0abd7194327b4659e7f554dc1996299e
Chris Zuber <shgysk8zer0@gmail.com>
2021-11-10 11:41:04 -0800

    Update generate.sh

--- img/jumplist/generate.sh
+++ img/jumplist/generate.sh
@@ -5,7 +5,7 @@ declare -r here=`dirname $0`
 
 if command -v inkscape &> /dev/null; then
 	for svg in $(ls ./${here}/*.svg); do
-		for size in (192 144 96 72 48 36); do
+		for size in "${sizes[@]}"; do
 			inkscape "${svg}" -h $size -e "${svg%.svg}-${size}.png"
 		done
 	done
