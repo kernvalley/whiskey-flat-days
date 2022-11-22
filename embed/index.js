b74f20f915e93c4effcf335bb1fb4964bc540e32
Chris Zuber <shgysk8zer0@gmail.com>
2022-11-21 21:29:47 -0800

    Add `<wfd-events>` as iframe

---
+++ embed/index.js
@@ -0,0 +1,25 @@
+customElements.whenDefined('wfd-events').then(() => {
+	const WFDEvents = customElements.get('wfd-events');
+	const params = new URLSearchParams(location.search);
+	const events = new WFDEvents();
+
+	if (params.has('souce')) {
+		events.source = params.get('source');
+	}
+
+	if (params.has('theme')) {
+		events.theme = params.get('theme');
+
+		try {
+			document.querySelector('meta[name="color-scheme"]')
+				.setAttribute('content', params.get('theme'));
+		} catch(err) {
+			console.error(err);
+		}
+	}
+
+	events.images = params.has('images');
+	events.style.height = '100vh';
+	events.style.width = '100%';
+	document.body.append(events);
+});
