c8b3900cb6d6a87b0607cf30070d847e0bb8a273
Chris Zuber <shgysk8zer0@gmail.com>
2022-12-12 13:04:05 -0800

    Update Products & Sellers

--- js/index.js
+++ js/index.js
@@ -301,31 +301,6 @@ Promise.all([
 		}
 	}
 
-	on('button.add-to-cart','click', () => {
-		const dialog = create('dialog', {
-			children: [
-				create('p', {
-					classList: ['status-box','info'],
-					text: 'WFD Store is in demo mode. Purchases are currently not enabled.'
-				}),
-				create('div', {
-					classList: ['center'],
-					children: [
-						create('button', {
-							classList: ['btn', 'btn-reject'],
-							text: 'Close',
-							events: { click: ({ target }) => target.closest('dialog').close() },
-						})
-					]
-				})
-			],
-			events: { close: ({ target }) => target.remove() },
-		});
-
-		document.body.append(dialog);
-		dialog.showModal();
-	});
-
 	on('[data-action]', 'click', ({ target }) => {
 		const { action } = target.closest('[data-action]').dataset;
 		switch (action.toLowerCase()) {
