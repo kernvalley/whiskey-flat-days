38b3e2ef1fd775446432cdb549fed131d9de0989
Chris Zuber <shgysk8zer0@gmail.com>
2023-02-11 16:40:19 -0800

    Fix RollUp config

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import urlResolve from 'rollup-plugin-url-resolve';
 import {terser} from 'rollup-plugin-terser';
 
 export default {
-	input: '_site/js/index.js',
+	input: 'js/index.js',
 	output: {
-		file: '_site/js/index.min.js',
+		file: 'js/index.min.js',
 		format: 'iife',
 		sourcemap: true,
 	},
