2cae184747d1ce5723e133ab5e287efc40dccb1d
Chris Zuber <shgysk8zer0@gmail.com>
2023-05-12 14:52:05 -0700

    Update import mapping of modules

--- js/handlers.js
+++ js/handlers.js
@@ -1,4 +1,4 @@
-import { each, attr } from 'https://cdn.kernvalley.us/js/std-js/dom.js';
+import { each, attr } from 'std-js/dom.js';
 
 export function  searchDateTimeRange({ from = new Date('2020-02-14T11:00'), hours = 2 } = {}) {
 	if (! (from instanceof Date)) {
